42dad6bcefddaf81022475adf8e9afa8d2682879
varje <varjekivila@gmail.com>
2025-08-21 21:57:01 +0300

    adding new event

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
 import {
-  QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
+import { queryClient } from './util/http.js';
 
 const router = createBrowserRouter([
   {
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-const queryClient = new QueryClient();
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
